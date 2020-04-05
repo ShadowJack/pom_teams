3c4f03f430842bb2ec01b459acafacb3a3bab257
shadowjack <kostyap3@gmail.com>
2020-04-05 14:52:17 +0300

    Try to parse activity

--- lib/pom_teams_web/controllers/activity_controller.ex
+++ lib/pom_teams_web/controllers/activity_controller.ex
@@ -3,11 +3,16 @@ defmodule PomTeamsWeb.ActivityController do
   Controller for handling conversations with MS Teams
   """
   use PomTeamsWeb, :controller
+  alias ExMicrosoftBot.Models.Activity
 
   require Logger
   
   def new(conn, params) do
     Logger.info(inspect(params))
+
+    activity = Activity.parse(params)
+    Logger.info(inspect(activity))
+
     send_resp(conn, 200, "")
   end
 end
